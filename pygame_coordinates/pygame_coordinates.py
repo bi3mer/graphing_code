2abac8a098da1f5bc839b294d69157ec4cc24b38
Colan Biemer <colan@comcast.net>
2018-07-08 17:26:45 -0700

    fixed naming and labeling mistakes

--- pygame_coordinates/pygame_coordinates.py
+++ pygame_coordinates/pygame_coordinates.py
@@ -13,14 +13,14 @@ y = [0, 1, 1, 0]
 plt.scatter(x, y)
 
 # labels
-plt.text(0.02, 0.02, r'(0,0)')
-plt.text(0.02, 0.94, r'(0, height)')
-plt.text(0.83, 0.02, r'(width,0)')
-plt.text(0.75, 0.94, r'(width, height)')
+plt.text(0.02, 0.02, r'(0,height)')
+plt.text(0.02, 0.94, r'(0, 0)')
+plt.text(0.75, 0.02, r'(width,height)')
+plt.text(0.81, 0.94, r'(0, height)')
 
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('Pygame coordinates')
 plt.grid(True)
-plt.savefig("matplotlib_coordinates.png")
+plt.savefig("pygame_coordinates.png")
 plt.show()
